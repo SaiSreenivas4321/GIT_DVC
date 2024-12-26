6a2a65a917297fbe4baa308ed57dbaac78d2075c
SaiSreenivas_nehask_Datascitist <sreenivassaibks@gmail.com>
2024-12-27 01:13:06 +0530

    added and checking and testing the v2 and v3 data

--- mt_code.py
+++ mt_code.py
@@ -13,6 +13,10 @@ df = pd.DataFrame(data)
 # adding a now row to df for vz SaiNehamehta
 new_row_loc = {'name': 'SaiNehamehta','age':30, 'city':"hyd"}
 df.loc[len(df.index)] = new_row_loc
+
+new_row_loc2= {'name': 'SaiNehamehta','age':30, 'city':"hyd","Status":"Both almost same and good friends sai marry a similar girl as life parnter"}
+df.loc[len(df.index)] = new_row_loc2
+
 data_dir = 'data'
 
 os.makedirs(data_dir,exist_ok=True)
